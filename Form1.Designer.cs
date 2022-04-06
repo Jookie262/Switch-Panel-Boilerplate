e192bc16d297be1e1c4a17d4dabdd77385f1900f
Jookie262 <barrientos.134038@iloilo.sti.edu.ph>
2022-04-06 13:24:02 +0800

    Customize the Form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,20 @@ namespace switch_panel_boilerplate
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(600, 401);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Switch Panel";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
